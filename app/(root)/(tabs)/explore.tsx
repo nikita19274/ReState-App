b735628bc68c3ca25c756fd4a5c1164bbe14deac
Nikita <example@gmail.com>
2025-03-25 20:25:10 -0400

    home ui

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,11 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Explore = () => {
   return (
-    <View>
-      <Text>Explore</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Explore</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,18 +1,88 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { FeaturedCard, Card } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { useGlobalContext } from "@/lib/global-provider";
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const { user } = useGlobalContext();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-lg my-10 font-rubik text-3xl">
-        Welcome to ReState
-      </Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={[1, 2]}
+        renderItem={({ item }) => <Card />}
+        keyExtractor={(item) => item.toString()}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <View className="flex flex-row items-center">
+                <Image
+                  source={{ uri: user?.avatar }}
+                  className="size-12 rounded-full"
+                />
+                <View className="flex flex-col items-start ml-2 justify-center">
+                  <Text className="text-xs font-rubik text-black-100">
+                    Good morning
+                  </Text>
+                  <Text className="text-base font-rubik-medium text-black-300">
+                    {user?.name}
+                  </Text>
+                </View>
+              </View>
+              <Image source={icons.bell} className="size-6" />
+            </View>
+            <Search />
+            <View className="my-5 ">
+              <View className="flex-row flex items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300 ">
+                  Featured
+                </Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300 ">
+                    See All
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <FlatList
+                data={[1, 2, 3]}
+                renderItem={({ item }) => <FeaturedCard />}
+                keyExtractor={(item) => item.toString()}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                bounces={false}
+                contentContainerClassName="flex gap-5 mt-5"
+              />
+            </View>
+            <View className="my-5 ">
+              <View className="flex-row flex items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300 ">
+                  Our Recomendation
+                </Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300 ">
+                    See All
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <Filters />
+          </View>
+        }
+      />
+    </SafeAreaView>
   );
 }
